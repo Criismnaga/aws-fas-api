1a2e6fa885720c03adc804b7d808e14ee3de6e15
cm@cesar.org.br <cm@cesar.org.br>
2025-09-24 11:29:01 -0300

    criação projeto

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensagem": "Bem-vindo à API da Cris!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
